04508b55cc4b5f4eeb86097bbfc63613613f79e9
Warren Fu <warrenfu@Warrens-MacBook-Air.local>
2025-12-16 07:41:37 -0500

    bruh

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -300,7 +300,7 @@ const Index = () => {
               )}
             </h1>
             <p className="text-xl md:text-2xl text-muted-foreground mb-10 max-w-2xl mx-auto animate-fade-in-up animation-delay-200">
-              A social platform we celebrate vulnerability, relate to rejection, and don't pretend to have it all figured out.
+              A social where we celebrate vulnerability, relate to rejection, and don't pretend to have it all figured out.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center animate-fade-in-up animation-delay-300">
               <Button size="xl" asChild>
